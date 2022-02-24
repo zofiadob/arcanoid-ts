aac15ae406df8b14494727b0e0fa5d35c2935ca9
zofiadob <z.dobrowolska24@gmail.com>
2022-02-24 19:14:07 +0100

    styling calc

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,9 @@
-import { question } from "readline-sync";
 
 type Operator = '+' | '-' | '*' | '/';
 
 function main():void{
-    const firstStr: string = question('Enter first number:\n');
-    const operator: string = question('Enter operator:\n');
-    const secondStr: string = question('Enter second number:\n');
 
-    const validInput = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
-
-    if(validInput){
-        const firstNum: number = parseInt(firstStr);
-        const secondNum: number = parseInt(secondStr);
-        const result = calculate(firstNum, operator as Operator, secondNum);
-        console.log(result);
-    }
-    else{
-        console.log('\nInvalid input!\n');
-    }
-
+   
 }
 
 function calculate(firstNum: number, operator: Operator, secondNum: number){
